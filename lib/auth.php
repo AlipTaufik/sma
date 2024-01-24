acf453f8eaaaf941f21e5dabc71fc0e1085c7e63
AlipTaufik11 <127857070+AlipTaufik@users.noreply.github.com>
2024-01-24 15:23:19 +0700

    Add files via upload

---
+++ lib/auth.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Filename : lib/auth.php
+Tools : SimplePHPBot
+Author : Freddy Wicaksono, M.Kom
+*/
+session_start();
+require_once __DIR__ . "/../vendor/autoload.php"; 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
+$base = $_ENV["BASE_URL"];
+if (!isset($_SESSION['email'])) {
+    header("Location: ".$base."login.php"); // Redirect to your login page
+    exit();
+}
+
+?>
